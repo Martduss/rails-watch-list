da197fdc304bcf6138aa5c18c34b69913e94f2c4
martin dussenneMartin Dussenne <martin.dussenne@gmail.com>
2025-11-06 16:47:42 +0100

    MVP de l'app

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,35 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(params[:id])
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+end
